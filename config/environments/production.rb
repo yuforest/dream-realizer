cde4bbd431264af2beccbcba2e37efb34940ffca
Yuta Omori <lostforest0@gmail.com>
2018-04-29 11:12:27 +0000

    second commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.serve_static_assets = true
   config.assets.compile = true
-  
+  config.assets.precompile +=  %w( *.woff *.eot *.svg *.ttf )
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
   # config.assets.precompile +=  %w( *.woff *.eot *.svg *.ttf )
