3bcb09561b729544bea782e6d8db48718beee959
Yuta Omori <lostforest0@gmail.com>
2018-05-04 17:42:14 +0000

    update!

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,8 +1,15 @@
 
+
+
 $(document).on('turbolinks:load', function() {
+    var size = $('.fc-toolbar').length
+    console.log(size)
+     if (size == 0){
     $('#calendar').fullCalendar({
        defaultView: 'month',
        events: '/calendars.json'
     });
+    };
+   
 });
 
